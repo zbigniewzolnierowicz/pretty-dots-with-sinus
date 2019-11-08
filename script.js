1726870607c0dadeab1e8d7cf8d8a41ef602f166
Glitch (hello-webpage) <none>
2019-11-08 18:33:54 +0000

    🍝📈 Checkpoint ./script.js:8411608/1337 ./index.html:8411608/25

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ let ctx
 if ('getContext' in canvas) {
   ctx = canvas.getContext('2d')
 }
-const AMOUNT_OF_TRIANGLES = 5
-const offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES
-const offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES
-for(let i = 0; i <= ; i+=offsetX) {
-  ctx.fillRect(i * offset, 0, 1, 1);
+const AMOUNT_OF_TRIANGLES = {
+  ROWS: 5,
+  COLUMNS: 10
+}
+const offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES.COLUMNS
+const offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES.ROWS
+for(let x = 0; x < ctx.canvas.width; x+=offsetX) {
+  for(let y = 0; y < ctx.canvas.height; y+=offsetY) {
+    ctx.moveTo(x, y)
+    ctx.beginPath()
+    ctx.lineTo(x + offsetX, y)
+    ctx.lineTo(x + offsetX, y + offsetY)
+    ctx.lineTo(x, y)
+    ctx.closePath()
+    ctx.fill()
+  }
 }
