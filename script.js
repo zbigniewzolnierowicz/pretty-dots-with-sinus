6b650badc39e2110b8e7010003e5760e7bf48b98
Glitch (hello-webpage) <none>
2019-11-08 20:07:55 +0000

    🎪🍫 Checkpoint ./script.js:8411608/1714

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if ('getContext' in canvas) {
 }
 const AMOUNT_OF_TRIANGLES = {
   ROWS: 5,
-  COLUMNS: 10
+  COLUMNS: 5
 }
 
 function randomIntFromInterval(min, max) {
@@ -16,6 +16,29 @@ function clear(ctx) {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 }
 
+function generateCircles(ctx) {
+  ctx.canvas.width = parseInt(window.getComputedStyle(ctx.canvas).width)
+  ctx.canvas.height = parseInt(window.getComputedStyle(ctx.canvas).height)
+  let offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES.COLUMNS
+  let offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES.ROWS
+  let circles = []
+  for(let x = 0; x < ctx.canvas.width; x+=offsetX) {
+    let row = []
+    for(let y = 0; y < ctx.canvas.height; y+=offsetY) {
+      let circle = {
+        center: {
+          x: x + parseInt(offsetX / 2),
+          y: y + parseInt(offsetY / 2)
+        },
+        radius: (Math.sin(x/ctx.canvas.width)*Math.sin(y/ctx.canvas.height))**2
+      }
+      row.push(circle)
+    }
+    circles.push(row)
+  }
+  return circles
+}
+
 function draw(ctx) {
   clear(ctx)
   ctx.canvas.width = parseInt(window.getComputedStyle(ctx.canvas).width)
@@ -24,14 +47,18 @@ function draw(ctx) {
   let offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES.ROWS
   for(let x = 0; x < ctx.canvas.width; x+=offsetX) {
     for(let y = 0; y < ctx.canvas.height; y+=offsetY) {
+      /*
       ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
       ctx.moveTo(x + parseInt(offsetX / 2), y + parseInt(offsetY / 2))
       ctx.beginPath()
-      ctx.arc(x + parseInt(offsetX / 2), y + parseInt(offsetY / 2), 10 * (Math.sin(x)*Math.sin(y))**2, 0, 2 * Math.PI)
+      ctx.arc(x + parseInt(offsetX / 2), y + parseInt(offsetY / 2), 10 * (Math.sin(x/ctx.canvas.width)*Math.sin(y/ctx.canvas.height))**2, 0, 2 * Math.PI)
       ctx.closePath()
-      ctx.fill()
+      ctx.fill() */
     }
   }
 }
 draw(ctx)
-window.addEventListener('resize', () => draw(ctx));
+window.addEventListener('resize', () => {
+  draw(ctx)
+  console.log(generateCircles(ctx))
+});
