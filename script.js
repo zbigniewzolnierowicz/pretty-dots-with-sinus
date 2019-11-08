9aa88810a1935c24d6d8eb9853f2d8160abc3d53
Glitch (hello-webpage) <none>
2019-11-08 18:22:53 +0000

    🚜🤗 Checkpoint ./script.js:8411608/430

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let canvas = document.querySelector('#canvas')
-console.log(canvas)
+let ctx
+if ('getContext' in canvas) {
+  ctx = canvas.getContext('2d')
+}
+const AMOUNT_OF_TRIANGLES = 5
+const offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES
+const offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES
+for(let i = 0; i <= ; i+=offsetX) {
+  ctx.fillRect(i * offset, 0, 1, 1);
+}
