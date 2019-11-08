a3dc5afbb88c3afdc64f02a5071115ee4d3809da
Glitch (hello-webpage) <none>
2019-11-08 18:44:54 +0000

    ☎️✊ Checkpoint ./script.js:8411608/1269 ./style.css:8411608/117

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ const AMOUNT_OF_TRIANGLES = {
   ROWS: 5,
   COLUMNS: 10
 }
-const offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES.COLUMNS
-const offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES.ROWS
+
+ctx.canvas.width = parseInt(window.getComputedStyle(ctx.canvas).width)
+ctx.canvas.height = parseInt(window.getComputedStyle(ctx.canvas).height)
+
+const offsetX = parseInt(window.getComputedStyle(ctx.canvas).width) / AMOUNT_OF_TRIANGLES.COLUMNS
+const offsetY = parseInt(window.getComputedStyle(ctx.canvas).height) / AMOUNT_OF_TRIANGLES.ROWS
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 for(let x = 0; x < ctx.canvas.width; x+=offsetX) {
   for(let y = 0; y < ctx.canvas.height; y+=offsetY) {
-    ctx.moveTo(x, y)
+    ctx.moveTo(x + parseInt(offsetX / 2), y + parseInt(offsetY / 2))
     ctx.beginPath()
-    ctx.lineTo(x + offsetX, y)
-    ctx.lineTo(x + offsetX, y + offsetY)
-    ctx.lineTo(x, y)
+    ctx.arc(x + parseInt(offsetX / 2), )
     ctx.closePath()
     ctx.fill()
   }
