88dd7846365cee09881587dc8e2fc0bbb9cb956c
Glitch (hello-webpage) <none>
2019-11-08 20:30:03 +0000

    🐺⏰ Checkpoint ./script.js:8411608/1289

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ if ('getContext' in canvas) {
   ctx = canvas.getContext('2d')
 }
 const AMOUNT_OF_TRIANGLES = {
-  ROWS: 5,
-  COLUMNS: 5
+  ROWS: 9,
+  COLUMNS: 16
 }
 
 function randomIntFromInterval(min, max) {
@@ -22,24 +22,22 @@ function generateCircles(ctx) {
   let offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES.COLUMNS
   let offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES.ROWS
   let circles = []
-  for(let x = 0; x < ctx.canvas.width; x+=offsetX) {
+  for(let x = 0; x <= ctx.canvas.width; x+=offsetX) {
     let row = []
-    for(let y = 0; y < ctx.canvas.height; y+=offsetY) {
+    for(let y = 0; y <= ctx.canvas.height; y+=offsetY) {
       let circle = {
         center: {
-          x: x + parseInt(offsetX / 2),
-          y: y + parseInt(offsetY / 2)
+          x: x,
+          y: y
         },
-        percents: {
-          x: x / ctx.canvas.width,
-          y: y / ctx.canvas.height
-        },
-        radius: 30 * Math.sin(Math.PI * x / ctx.canvas.width) * Math.sin(Math.PI * y / ctx.canvas.height)
+        radius: Math.floor(30 * (Math.sin(Math.PI * x / ctx.canvas.width) * Math.sin(Math.PI * y / ctx.canvas.height)) + 1_row.shift())
       }
       row.push(circle)
     }
+    // row.shift()
     circles.push(row)
   }
+  // circles.shift()
   return circles
 }
 
@@ -49,29 +47,21 @@ function draw(ctx, circles) {
   ctx.canvas.height = parseInt(window.getComputedStyle(ctx.canvas).height)
   let offsetX = ctx.canvas.width / AMOUNT_OF_TRIANGLES.COLUMNS
   let offsetY = ctx.canvas.height / AMOUNT_OF_TRIANGLES.ROWS
-  for(let x = 0; x < ctx.canvas.width; x+=offsetX) {
-    for(let y = 0; y < ctx.canvas.height; y+=offsetY) {
-      /*
+  for (let row of circles) {
+    for (let circle of row) {
+      ctx.moveTo(0,0)
       ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
-      ctx.moveTo(x + parseInt(offsetX / 2), y + parseInt(offsetY / 2))
-      ctx.beginPath()
-      ctx.arc(x + parseInt(offsetX / 2), y + parseInt(offsetY / 2), 10 * (Math.sin(x/ctx.canvas.width)*Math.sin(y/ctx.canvas.height))**2, 0, 2 * Math.PI)
-      ctx.closePath()
-      ctx.fill() */
-    }
-  }
-  for (row of circles) {
-      for (circle of row) {
-      ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
-      ctx.moveTo(circle.center.x, circle.center.y)
       ctx.beginPath()
       ctx.arc(circle.center.x, circle.center.y, circle.radius, 0, 2 * Math.PI)
       ctx.closePath()
       ctx.fill()
-    }) 
+    }
   }
+  console.log(circles)
 }
-draw(ctx)
+let circles = generateCircles(ctx)
+draw(ctx, circles)
 window.addEventListener('resize', () => {
-  draw(ctx, generateCircles(ctx))
+  let circles = generateCircles(ctx)
+  draw(ctx, circles)
 });
